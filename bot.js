dd400715b7b887cfc02f76bd7e35172442960f86
Milk-Cool <coder.tomsk@gmail.com>
2025-06-29 10:20:53 +0300

    database and slash commands

--- bot.js
+++ bot.js
@@ -1,10 +1,54 @@
 import "dotenv/config";
 import { WebClient } from "@slack/web-api";
-import { FEED_ID, parseMsg } from "./index.js";
+import { SocketModeClient } from "@slack/socket-mode";
+import { db, deleteSubscription, FEED_ID, getAllSubscribers, getSubscriptions, parseMsg, pushSubscription } from "./index.js";
 
-const client = new WebClient(process.env.SLACK_TOKEN);
+const socket = new SocketModeClient({ appToken: process.env.SLACK_APP_TOKEN });
+const client = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+const MENTION_REGEX = /(?<=<@)[^|>]+(?=\||>)/;
+
+const errors = {
+    invalidMention: "Please specify who you want to subscribe to as a Slack mention.",
+    alreadySubscribed: "Aleady subscribed!",
+    notSubscribed: "Not subscribed!"
+};
+
+socket.on("slash_commands", async ({ body, ack, say }) => {
+    if(body.command === "/dsubscribe") {
+        const who = body.text.match(MENTION_REGEX)?.[0];
+        if(!who) return await ack({ text: errors.invalidMention });
+        if(!pushSubscription(body.user_id, who, body.channel_id)) return await ack({ text: errors.alreadySubscribed });
+        await ack({ text: "Subscribed!" });
+    } else if(body.command === "/dunsubscribe") {
+        const who = body.text.match(MENTION_REGEX)?.[0];
+        if(!who) return await ack({ text: errors.invalidMention });
+        if(!deleteSubscription(body.user_id, who)) return await ack({ text: errors.notSubscribed });
+        await ack({ text: "Unsubscribed!" });
+    } else if(body.command === "/dsubscriptions") {
+        const subscriptions = getSubscriptions(body.user_id);
+        await ack({ text: "Your subscriptions:\n" + (subscriptions.map(x => `<@${x.who}>`).join("\n") || "None yet!") });
+    } else if(body.command === "/dsubscribers") {
+        const subscriptions = getAllSubscribers(body.user_id);
+        await ack({ text: "Your subscribers:\n" + (subscriptions.map(x => `<@${x.subscriber}>`).join("\n") || "None yet!") });
+    }
+});
+(async () => await socket.start())();
 
 const result = await client.conversations.history({
     channel: FEED_ID
 });
-console.log(result.messages.map(x => parseMsg(x.blocks[0].text.text, x?.root?.blocks?.[0]?.text?.text)));
+console.log(result.messages.map(x => parseMsg(x?.blocks?.[0]?.text?.text, x?.root?.blocks?.[0]?.text?.text)));
+
+const stop = () => {
+    db.close();
+    process.exit(0);
+}
+
+process.on("SIGHUP", () => stop());
+process.on("SIGUSR2", () => stop());
+process.on("SIGINT", () => stop());
+process.on("SIGTERM", () => stop());
+
+process.on("uncaughtException", e => console.error(e));
+process.on("unhandledRejection", e => console.error(e));
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+import Database from "better-sqlite3";
+
+export const db = new Database("data.db");
+db.pragma("journal_mode = WAL");
+
+/**
+ * @typedef {object} Subscription
+ * @prop {number} id
+ * @prop {string} subscriber
+ * @prop {string} who
+ */
+db.prepare(`CREATE TABLE IF NOT EXISTS subscriptions (
+    id INTEGER PRIMARY KEY,
+    subscriber TEXT NOT NULL,
+    dm_channel_id TEXT NOT NULL,
+    who TEXT NOT NULL
+)`).run();
+db.prepare(`CREATE TABLE IF NOT EXISTS timestamp (
+    timestamp REAL
+)`);
+
 export const FEED_ID = "C091CEEHJ9K"; // #summer-of-making-feed
 
 /**
@@ -10,7 +31,65 @@ export const splitFirstNewline = text => {
     return [text.slice(0, index), text.slice(index + 1)];
 }
 
-// TODO
+/**
+ * Gets a subscription.
+ * @param {string} subscriber Subscriber ID 
+ * @param {string} who Who the subscriber is subscribing to (ID)
+ * @returns {Subscription} The subscription
+ */
+export function getSubscription(subscriber, who) {
+    return db.prepare(`SELECT * FROM subscriptions WHERE subscriber = ? AND who = ?`)
+        .get(subscriber, who);
+}
+
+/**
+ * Gets all subscriptions to a specific person.
+ * @param {string} who Who the subscribers are subscribed to (ID)
+ * @returns {Subscription[]} The subscription
+ */
+export function getAllSubscribers(who) {
+    return db.prepare(`SELECT * FROM subscriptions WHERE who = ?`)
+        .all(who);
+}
+
+/**
+ * Gets all subscriptions from a specific person.
+ * @param {string} subscriber Who the subscribers are subscribed to (ID)
+ * @returns {Subscription[]} The subscriptions
+ */
+export function getSubscriptions(subscriber) {
+    return db.prepare(`SELECT * FROM subscriptions WHERE subscriber = ?`)
+        .all(subscriber);
+}
+
+/**
+ * Creates a new subscription if it doesn't exist.
+ * @param {string} subscriber Subscriber ID 
+ * @param {string} who Who the subscriber is subscribing to (ID)
+ * @param {string} dmChannelID DM channel ID of the subscriber
+ * @returns {boolean} `false` if it already exists
+ */
+export function pushSubscription(subscriber, who, dmChannelID) {
+    if(getSubscription(subscriber, who))
+        return false;
+    db.prepare(`INSERT INTO subscriptions (subscriber, who, dm_channel_id) VALUES (?, ?, ?)`)
+        .run(subscriber, who, dmChannelID);
+    return true;
+}
+
+/**
+ * Deletes a subscription.
+ * @param {string} subscriber Subscriber ID 
+ * @param {string} who Who the subscriber is subscribing to (ID)
+ * @returns {boolean} `false` if it didn't exist
+ */
+export function deleteSubscription(subscriber, who) {
+    if(!getSubscription(subscriber, who))
+        return false;
+    db.prepare(`DELETE FROM subscriptions WHERE subscriber = ? AND who = ?`)
+        .run(subscriber, who);
+    return true;
+}
 
 /** @typedef {["project" | "update", string, string, string] | null} ParsedMessage */
 
@@ -23,7 +102,6 @@ const parseRoot = text => {
     const [_firstLine, rest] = splitFirstNewline(text);
     let [name, description] = splitFirstNewline(rest);
     const id = description.match(ID_REGEX)?.[0];
-    console.log(id);
     if(!id) return null;
     description = description.replace(/\s*by\s*<@[^>]+>$/, "");
     return ["project", id, name, description];
@@ -36,6 +114,7 @@ const parseRoot = text => {
  * @returns {ParsedMessage} [type, author ID, name, contents] or null if unparseable
  */
 export const parseMsg = (text, root) => {
+    if(!text) return null;
     if(!root) {
         return parseRoot(text);
     } else {
